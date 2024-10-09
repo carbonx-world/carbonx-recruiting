32851149570ddf0eceb509fd21ecbc9305e8e2aa
joe arlia <joearlia@joes-MacBook-Pro-2.local>
2024-10-08 21:15:45 -0400

    adding questions

---
+++ question_2.py
@@ -0,0 +1,8 @@
+"""
+Remove Duplicates from a List
+Problem: Write a function that takes a list and returns a new list with all duplicate values removed.
+
+Example:
+Input: [1, 2, 2, 3, 4, 4, 5]
+Output: [1, 2, 3, 4, 5]
+"""
